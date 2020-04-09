097d57e3d9777b880b2fea35fd90b54353a48aef
LURKS02 <dertflag@naver.com>
2020-04-10 00:12:00 +0900

    test

---
+++ day01/src/step1_02/op/OpEx01.java
@@ -0,0 +1,23 @@
+package step1_02.op;
+
+public class OpEx01 {
+
+	public static void main(String[] args) {
+		//더하기
+		System.out.println(10 + 3);
+		//빼기
+		System.out.println(10 - 3);
+		//곱하기
+		System.out.println(10 * 3);
+		//나누기
+				
+		// 정수 / 정수 = 정수
+		// 정수 / 실수 = 실수
+				
+		System.out.println(10 / 3);
+		System.out.println(10 / 3.0);
+		System.out.println(10 % 3);
+				
+	}
+
+}
---
+++ day01/src/step1_02/op/OpEx02.java
@@ -0,0 +1,23 @@
+package step1_02.op;
+
+public class OpEx02 {
+
+	public static void main(String[] args) {
+		//덧셈 연산자
+		System.out.println(10 + 3);
+				
+		//연결 연산자
+				
+		// 문자 + 문자 = 문자
+		// 숫자 + 문자 = 문자
+				
+		System.out.println("안녕" + "하세요");
+		System.out.println("10" + 3);
+		System.out.println(3 + "10");
+				
+		System.out.println("더하기 = " + 10 + 3);
+		System.out.println("더하기 = " + (10 + 3));
+
+	}
+
+}
