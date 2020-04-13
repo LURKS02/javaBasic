c182ccff146011d8b9a478e1ee3340384a5c096e
LURKS02 <thecrooks2@gmail.com>
2020-04-14 00:13:23 +0900

    java while ex taxi

--- day01/src/step1_10/whileEx/WhileEx18_문제.java
+++ day01/src/step1_10/whileEx/WhileEx18_문제.java
@@ -1,22 +1,36 @@
 package step1_10.whileEx;
 import java.util.Scanner;
+import java.util.Random;
+
+//택시 서비스
+//-10 ~ 10 사이의 랜덤 숫자 2개를 저장해 목적지로 설정
+//속도: 1~3까지 설정
+//방향 : 동1서2남3북4
+//이동 : 설정 방향으로 설정된 속도만큼 이동
+//거리 2칸 당 50원씩 추가되어 요금 출력
 
 public class WhileEx18_문제 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
 		
-		int desX = 0;
-		int desY = 0;
+		//목적지
+		int desX = ran.nextInt(21) - 10;
+		int desY = ran.nextInt(21) - 10;
 		
+		//현재 위치
 		int x = 0;
 		int y = 0;
 		
+		//방향
 		int dir = 0;
-		
+		//속도
 		int speed = 0;
-		
+		//요금
 		int fee = 0;
+		//이동 칸 수
+		int move = 0;
 		
 		while(true) {
 			System.out.println("택시 서비스");
@@ -30,13 +44,29 @@ public class WhileEx18_문제 {
 			int sel = scan.nextInt();
 			
 			if (sel == 1) {
-		
+				System.out.println("방향을 설정하세요.");
+				dir = scan.nextInt();
 			}
 			else if (sel == 2) {
-				
+				System.out.println("속도를 설정하세요.");
+				speed = scan.nextInt();
 			}
 			else {
+				System.out.println("이동합니다.");
+				if(dir == 1) x += speed;
+				else if(dir == 2) x -= speed;
+				else if(dir == 3) y -= speed;
+				else y += speed;
+				move += speed;
+			}
+			
+			if (x == desX && y == desY) {
+				if (move % 2 == 0) fee = (move / 2) * 50;
+				else fee = (move / 2) * 50 + 50;
 				
+				System.out.println("목적지 도착");
+				System.out.println("요금 : " + fee);
+				break;
 			}
 		}
 	}
