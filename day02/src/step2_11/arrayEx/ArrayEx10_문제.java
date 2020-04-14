dbe053de4d11e10a383f35fc8aa1bf3d0fb021c2
LURKS02 <thecrooks2@gmail.com>
2020-04-14 22:53:33 +0900

    java array ex3

---
+++ day02/src/step2_11/arrayEx/ArrayEx10_문제.java
@@ -0,0 +1,70 @@
+package step2_11.arrayEx;
+
+import java.util.Scanner;
+
+/*
+ * # 즉석복권
+ * 1. 숫자 7이 연속으로 3번 등장하면, 당첨복권이다.
+ * 2. 아래 3종류의 복권의 당첨여부를 출력한다.
+ */
+
+public class ArrayEx10_문제 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int[] lotto1 = {0, 0, 7, 7, 7, 0, 0, 0};
+		int[] lotto2 = {7, 0, 7, 7, 0, 0, 0, 0};
+		int[] lotto3 = {7, 0, 7, 7, 7, 0, 7, 0};
+		
+		while(true) {
+			
+			System.out.println("[1]번복권 결과확인");
+			System.out.println("[2]번복권 결과확인");
+			System.out.println("[3]번복권 결과확인");
+			
+			System.out.print("메뉴 선택 : ");
+			int sel = scan.nextInt();
+			int get = 0;
+			
+			if(sel == 1) {
+				for (int i = 0; i < 6; i++) {
+					if (lotto1[i] == 7) {
+						if (lotto1[i+1] == 7 && lotto1[i+2] == 7) {
+							System.out.println("당첨입니다.");
+							get = 1;
+						}
+					}
+				}
+				if(get == 0) System.out.println("꽝입니다.");
+			}
+			
+			else if(sel == 2) {
+				for (int i = 0; i < 6; i++) {
+					if (lotto2[i] == 7) {
+						if (lotto2[i+1] == 7 && lotto2[i+2] == 7) {
+							System.out.println("당첨입니다.");
+							get = 1;
+						}
+					}
+				}
+				if(get == 0) System.out.println("꽝입니다.");
+			}
+			
+			else if(sel == 3) {
+				for (int i = 0; i < 6; i++) {
+					if (lotto3[i] == 7) {
+						if (lotto3[i+1] == 7 && lotto3[i+2] == 7) {
+							System.out.println("당첨입니다.");
+							get = 1;
+						}
+					}
+				}
+				if(get == 0) System.out.println("꽝입니다.");
+			}
+		}
+
+	}
+
+}
---
+++ day02/src/step2_11/arrayEx/ArrayEx11_문제.java
@@ -0,0 +1,46 @@
+package step2_11.arrayEx;
+import java.util.Random;
+
+/*
+ * # 중복숫자 금지[1단계]
+ * 1. 0~4 사이의 숫자를 arr배열에 저장한다.
+ * 2. 단, 중복되는 숫자는 없어야 한다.
+ * 힌트) 랜덤 숫자를 check배열의 인덱스로 활용한다.
+ * 
+ * 예)
+ * 랜덤숫자 : 1
+ * check = {0, 1, 0, 0, 0}
+ * arr   = {1, 0, 0, 0, 0}
+ * 랜덤숫자 : 3
+ * check = {0, 1, 0, 1, 0}
+ * arr   = {1, 3, 0, 0, 0}
+ * 랜덤숫자 : 2
+ * check = {0, 1, 1, 1, 0}
+ * arr   = {1, 3, 2, 0, 0}
+ */
+
+public class ArrayEx11_문제 {
+
+	public static void main(String[] args) {
+		
+		Random ran = new Random();
+		int[] check = new int[5];
+		int[] arr = new int[5];
+		
+		int k = 0;
+		for (int i = 0; i < 5; i++) {
+			int num = ran.nextInt(5);
+			if(check[num] == 1) {
+				i -= 1;
+				System.out.println("중복 숫자");
+			}
+			else {
+				arr[k++] = num;
+				check[num] = 1;
+			}
+		}
+		for (int i = 0; i < 5; i++)
+			System.out.println(arr[i] + " ");
+	}
+
+}
