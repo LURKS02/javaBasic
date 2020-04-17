70ad0722f7a2ad621e419d0764a998e4003d31ff
LURKS02 <thecrooks2@gmail.com>
2020-04-18 04:09:22 +0900

    edit

--- day01/src/step1_10/whileEx/WhileEx23_문제.java
+++ day01/src/step1_10/whileEx/WhileEx23_문제.java
@@ -2,13 +2,8 @@ package step1_10.whileEx;
 
 import java.util.Scanner;
 
-/*
- * # 소수찾기[2단계]
- * 정수 한 개를 입력받아, 2부터 해당 숫자까지의 모든 소수 출력
- * 예)
- * 입력 : 20
- * 2, 3, 5, 7, 11, 13, 17, 19
- */
+// 소수찾기[2단계]
+ 
 
 public class WhileEx23_문제 {
 
