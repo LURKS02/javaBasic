48c3a4421186526572ef94b09ecb9e22b8c1d76c
LURKS02 <thecrooks2@gmail.com>
2020-04-27 14:30:37 +0900

    edit

--- day03/src/step3_13/arrayEx/ArrayEx28_문제.java
+++ day03/src/step3_13/arrayEx/ArrayEx28_문제.java
@@ -3,14 +3,8 @@ package step3_13.arrayEx;
 import java.util.Scanner;
 import java.util.Random;
 
-/* 
- * # 1 to 50[3단계] : 1 to 18
- * 1. 구글에서 1 to 50 이라고 검색한다.
- * 2. 1 to 50 순발력 게임을 선택해 게임을 실습해본다.
- * 3. 위 게임을 숫자범위를 좁혀 1 to 18로 직접 구현한다.
- * 4. 숫자 1~9는 front 배열에 저장하고,
- *    숫자 10~18은 back 배열에 저장한다.
- */
+//1 to 50[3단계] : 1 to 18
+
 
 public class ArrayEx28_문제 {
 
